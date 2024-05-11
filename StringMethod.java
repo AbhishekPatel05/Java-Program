faa838e4d8f56f58e39592c8d290dc636c053b80
AbhishekPatel05 <abhishekpatel05022000@gmail.com>
2024-05-11 14:28:47 +0530

    Modify

--- StringMethod.java
+++ StringMethod.java
@@ -18,6 +18,24 @@ System.out.println(myArray);
 String myStr = "Hello, World!";
 myStr.getChars(0, 5, myArray, 2);
 System.out.println(myArray);
+
+
+
+char[] A = {'0', '1', '2', '3', '4', '5', '6', '7' };
+System.out.println(A);
+
+String S = "Abhishek";
+S.getChars(0, 4 , A, 2);
+System.out.println(A);
+
+
+//-------charAt-------
+
+String a = "Abhishek";
+System.out.println(a.charAt(4));
+
+
+
 }
 }
 
